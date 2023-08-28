a4ac7e8ea29261b71b28dfb2ef3382d845e4699d
Rip Britton <ripbritton3@gmail.com>
2023-08-28 09:52:08 -0500

    Initial commit

---
+++ PokeFusionBot/ConfigurationManager.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Configuration;
+
+public class ConfigUtility
+{
+    public static async Task<string> GetToken()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
+        var config = builder.Build();
+        var tokenFileLocation = config["AppSettings:TokenFileLocation"];
+        Console.WriteLine(tokenFileLocation);
+        var token = await File.ReadAllTextAsync(tokenFileLocation);
+        return token;
+    }
+}

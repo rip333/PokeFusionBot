8327d56c04efdf8d5d6abf8fb28792ef5c479cd9
Rip Britton <ripbritton3@gmail.com>
2023-08-29 13:06:01 -0500

    trying old nuget package

--- PokeFusionBot/Program.cs
+++ PokeFusionBot/Program.cs
@@ -1,23 +1,34 @@
-﻿using TelegramBotBase.Builder;
+﻿using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("PokeFusionBot, I choose you!");
 
+var updateManager = new UpdateManager();
 var token = await ConfigUtility.GetToken();
+TelegramBotClient _bot;
+_bot = new TelegramBotClient(token);
+var me = await _bot.GetMeAsync();
+Console.Title = me.Username ?? "Telegram Bot";
+using var cts = new CancellationTokenSource();
 
-var bot = BotBaseBuilder
-    .Create()
-    .WithAPIKey(token)
-    .DefaultMessageLoop()
-    .WithStartForm<StartForm>()
-    .NoProxy()
-    .CustomCommands(a => {})
-    .NoSerialization()
-    .UseEnglish()
-    .Build();
+while (true)
+{
+    try
+    {
+        // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+        _bot.StartReceiving(
+            new DefaultUpdateHandler(updateManager.HandleUpdateAsync, updateManager.HandleErrorAsync),
+            cts.Token);
 
-await bot.UploadBotCommands();
-
-// Start your Bot
-await bot.Start();
-Console.ReadLine();
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.Read();
+        // Send cancellation request to stop bot
+        cts.Cancel();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        Thread.Sleep(60000);
+    }
+}
